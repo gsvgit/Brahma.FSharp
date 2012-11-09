ce82b5de08af5da19835e92b8a38d58c8c165cbd
rsdpisuy <rsdpisuy@gmail.com>
2012-11-09 20:29:58 +0400

    Copyright headers.

--- Source/Brahma.FSharp.OpenCL.Translator/Type.fs
+++ Source/Brahma.FSharp.OpenCL.Translator/Type.fs
@@ -1,4 +1,19 @@
-﻿module Brahma.FSharp.OpenCL.Translator.Type
+﻿// Copyright (c) 2012 Semyon Grigorev <rsdpisuy@gmail.com>
+// All rights reserved.
+// 
+// The contents of this file are made available under the terms of the
+// Eclipse Public License v1.0 (the "License") which accompanies this
+// distribution, and is available at the following URL:
+// http://www.opensource.org/licenses/eclipse-1.0.php
+// 
+// Software distributed under the License is distributed on an "AS IS" basis,
+// WITHOUT WARRANTY OF ANY KIND, either expressed or implied. See the License for
+// the specific language governing rights and limitations under the License.
+// 
+// By using this software in any fashion, you are agreeing to be bound by the
+// terms of the License.
+
+module Brahma.FSharp.OpenCL.Translator.Type
 
 open Brahma.FSharp.OpenCL.AST
 
