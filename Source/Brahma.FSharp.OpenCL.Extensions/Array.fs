0689418ec7088213992f99c967c702476ab09477
rsdpisuy <rsdpisuy@gmail.com>
2013-01-21 13:38:04 +0400

    Fix buffer to array reading.

--- Source/Brahma.FSharp.OpenCL.Extensions/Array.fs
+++ Source/Brahma.FSharp.OpenCL.Extensions/Array.fs
@@ -24,4 +24,4 @@ type ``[]``<'t> with
     member this.ToHost(kernel:ICLKernel) =
         new Commands.ReadBuffer<'t>(kernel.AutoconfiguredBuffers.[this], Marshal.SizeOf(typeof<'t>),true,0,this.Length,this)
     member this.ToHost(kernel:ICLKernel, hostArray:array<'t>) =
-        new Commands.ReadBuffer<'t>(kernel.AutoconfiguredBuffers.[this], Marshal.SizeOf(typeof<'t>),true,0,hostArray.Length,hostArray)
+        new Commands.ReadBuffer<'t>(kernel.AutoconfiguredBuffers.[this], Marshal.SizeOf(typeof<'t>),true,0,(min hostArray.Length this.Length),hostArray)
