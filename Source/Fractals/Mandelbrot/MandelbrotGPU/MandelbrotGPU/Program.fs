395ce9cb65da898cd8ee2462007eb35e593374fe
AndreyBulgakov <pivolit@gmail.com>
2014-03-31 22:44:09 +0400

    Rabotaet. Pochti.

--- Source/Fractals/Mandelbrot/MandelbrotGPU/MandelbrotGPU/Program.fs
+++ Source/Fractals/Mandelbrot/MandelbrotGPU/MandelbrotGPU/Program.fs
@@ -5,8 +5,10 @@ open Brahma.FSharp.OpenCL.Core
 open Microsoft.FSharp.Quotations
 open Brahma.FSharp.OpenCL.Extensions
 
+open System.Drawing
+open System.Windows.Forms 
 let Main =    
-    let localWorkSize = 1
+    let localWorkSize = 10
     let deviceType = DeviceType.Gpu
 
     let provider =
@@ -16,62 +18,98 @@ let Main =
 
     printfn "Using %A" provider
     let mutable commandQueue = new CommandQueue(provider, provider.Devices |> Seq.head)
-    let cParallel: array<int> = Array.zeroCreate 400
-    let cParallel2: array<int> = Array.zeroCreate 400
+    let cParallel: array<int> = Array.zeroCreate 160000
     let command = 
         <@                            
-            fun (r:_2D) (cx:array<_>) (cy:array<_>) ->                
+            fun (r:_2D)  (cx:array<_>) ->                
                 let x = r.GlobalID0
                 let y = r.GlobalID1
                 let scaling = 0.5 
-                let size : float = 100.0
-                let fx = float x / size * scaling + float -1.5
-                let fy = float y / size * scaling + float -1.0
-                let maxr : float= 1.0
-                let maxi : float= 1.0
-                let minr : float= -1.78
-                let mini : float= -1.0
-                let zr : float = 0.0
-                let zi : float= 0.0
-                let cr = float x  //20
-                let ci = float y
+                let size = 100.0
+                let fx = float x / size * scaling +  -1.5
+                let fy = float y / size * scaling +  -1.0
+                let maxr = 1.0
+                let maxi = 1.0
+                let minr = -1.78
+                let mini = -1.0
+                let zr = 0.0
+                let zi = 0.0
+                let cr = fx
+                let ci = fy
                 let iter = 20
-                let mutable flag = true
+                let mutable flag = false
+                let mutable flag2 = true
                 let mutable zr1 = zr
                 let mutable zi1 = zi
-                let mutable count = 0
-                while flag && (count <= iter) do
-                    if (minr < zr1) && (mini < zi1) && (zr1 < maxr) && (zi1 < maxi)
+                let mutable count = 0;
+                while (flag2) do
+                    if (minr < zr1) && (mini < zi1) && (zr1 < maxr) && (zi1 < maxi) && (count < iter)
                     then
                         count <- count + 1
-                        zr1 <- zr1 * zr1 - zi1 * zi1 + cr
-                        zi1 <- zr1 * zi1 + zi1 * zr1 + ci
+                        zr1 <- (zr1*zr1 - zi1*zi1 + cr)
+                        zi1 <- (zr1*zi1 + zi1*zr1 + ci)
                     else
-                        flag <- false
-
-                if flag  //((%mandl) 0.0  0.0 (float x) (float y) 20)
+                        flag2 <- false 
+                        if count=iter
+                        then flag <- true  
+                        else flag <- false                
+                if flag 
                 then
-                    cx.[x] <- x
-                    cy.[x] <- y  
-                else
-                    cx.[x] <- 0
-                    cy.[x] <- 0  
+                    cx.[x*400+y] <- 1
+                else 
+                    cx.[x*400+y] <- 0  
         @>
     printf "Start1"
     let kernel, kernelPrepare, kernelRun = provider.Compile command
     printf "Start2"
-    let d =(new _2D(cParallel.Length, cParallel.Length, localWorkSize, localWorkSize))
+    let d =(new _2D(400, 400, localWorkSize, localWorkSize))
 
-    kernelPrepare d cParallel cParallel2
+    kernelPrepare d cParallel
     let _ = commandQueue.Add(kernelRun()).Finish()            
     let _ = commandQueue.Add(cParallel.ToHost provider).Finish()
-    let _ = commandQueue.Add(cParallel2.ToHost provider).Finish()
     commandQueue.Dispose()
     provider.Dispose()
     provider.CloseAllBuffers()
     //ignore (System.Console.Read())
-    (cParallel, cParallel2)
-printfn "%A" Main
+    cParallel
+//let f (a:array<int>) =
+//    for i in 0..(a.Length-1) do
+//        printf "%A" a.[i]
+//f Main
+
+let draw  (image:Bitmap) (arr1:array<int>) =
+    let mutable t = -400; 
+    for x = 0 to 399 do
+        t <- t + 400
+        for y = 0 to 399 do 
+            image.SetPixel(x, y,  if (arr1.[t+y] <> 0) then  Color.Black else Color.White)
+
+let form =
+    let temp = new Form()
+    let image = new Bitmap(400, 400);
+    draw image Main
+    temp.Paint.Add(fun e -> e.Graphics.DrawImage(image, 0, 0))
+    temp.SetBounds(400, 400, 410, 450)
+    temp
+
+do Application.Run(form);;
+
+//let form =
+//   let scaling = 0.5 
+//   let size = 100.0
+//   let image = new Bitmap(400, 400)
+//   for x = 0 to (image.Height-1) do
+//     for y = 0 to (image.Width-1) do
+//       let fx = (float x)/size * scaling + (-1.5)
+//       let fy = (float y)/size * scaling + (-1.0)
+//       //let t = move((float)x, (float)y,-1.5, -1.0)
+//       //image.SetPixel(x, y, if (mandl (0.0, 0.0) t 20 0) then Color.Black else Color.White)
+//       image.SetPixel(x, y, if (mandl 0.0 0.0 fx fy 20) then Color.Black else Color.White)
+//   let temp = new Form()
+//   temp.Paint.Add(fun e -> e.Graphics.DrawImage(image, 0, 0))
+//   temp.SetBounds(400, 400, 410, 450)
+//   temp
+
 
 //    let rec mandl zr zi cr ci iter count =
 //        if (minr < zr) && (mini < zi) && (zr < maxr) && (zi < maxi) && (count < iter) then
