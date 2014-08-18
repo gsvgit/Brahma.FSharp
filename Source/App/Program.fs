b9cd8bd38e7533732ceafe98db500c8946ea9ded
gsv <rsdpisuy@gmail.com>
2014-08-18 12:24:46 +0400

    Refactoring. Less generics. 2-blocks example

--- Source/App/Program.fs
+++ Source/App/Program.fs
@@ -3,8 +3,15 @@ open NA.IL
 open System.Collections.Generic
 
 let board =
-    let blockCount = 1
-    let blocks = Array.init blockCount (fun _ -> new Block<_,int,int,int,int,int,int>((+),(-),(*),(/)))
+    let blockCount = 2
+    let intBocks = Array.init blockCount (fun _ -> new Block<_> [|(+);(-);(*);(/)|])
+    let con1:Connector<int,int> = new Connector<_,_>(id)
+    let con2:Connector<int,int> = new Connector<_,_>(id)
+    intBocks.[0].InConnectors <- [|fun _ -> con1.OutBuf|]
+    intBocks.[1].InConnectors <- [|fun _ -> con2.OutBuf|]
+    intBocks.[0].OutConnectors <- [|con2.Move|]
+    intBocks.[1].OutConnectors <- [|con1.Move|]
+    let blocks = intBocks
     blocks
 
 let run (commands:array<array<_>>) =
@@ -12,7 +19,6 @@ let run (commands:array<array<_>>) =
         Array.init 
             commands.Length 
             (fun i -> 
-                //for c in commands.[i] do board.[i].Instructions.Enqueue c
                 board.[i].Instructions <- new Queue<_>(commands.[i])
                 board.[i].Instructions )
     while queues |> Array.forall (fun q -> q.Count > 0) do
@@ -28,6 +34,17 @@ let commands =
         Set (Const 2, OpBlock(0,1))
         Move (OpBlock(0,0), OpBlock(0,1))
         Move (OpBlock(0,1), OpBlock(2,0))
+        Eps
+        Move (Connector 0, OpBlock(2,0))
+        |]
+        [|
+        Set (Const 3, OpBlock(0,0))        
+        Set (Const 4, OpBlock(2,0))
+        Set (Const 2, OpBlock(0,1))
+        Move (OpBlock(0,0), OpBlock(0,1))
+        Move (OpBlock(0,1), OpBlock(2,0))
+        Move (OpBlock(2,0), Connector(0))
+        Eps
         |]
     |]
 
--- Source/Elements/Elements.fs
+++ Source/Elements/Elements.fs
@@ -5,18 +5,8 @@ open NA.IL
 let opBlockSuze = 256
 
 type Connector<'a,'b>(onTransfer:'a ->'b) =
-    member this.OnTransfer = onTransfer
     member val OutBuf = Unchecked.defaultof<'b> with get, set
-    member this.Move v = this.OutBuf <- this.OnTransfer v
-
-[<Struct>]
-type ConnectorsBlock<'a0,'b0,'a1,'b1,'a2,'b2,'a3,'b3,'a4,'b4,'a5,'b5> =
-    val c0:Connector<'a0,'b0>
-    val c1:Connector<'a1,'b1>
-    val c2:Connector<'a2,'b2>
-    val c3:Connector<'a3,'b3>
-    val c4:Connector<'a4,'b4>
-    val c5:Connector<'a5,'b5>
+    member this.Move v = this.OutBuf <- onTransfer v
 
 type OperationBlock<'a>(op: 'a -> 'a -> 'a) =
     let data = Array.init opBlockSuze (fun _ -> Unchecked.defaultof<'a>)
@@ -24,156 +14,49 @@ type OperationBlock<'a>(op: 'a -> 'a -> 'a) =
     member this.Set i v = data.[i] <- v
     member this.Get i = data.[i]
 
-type Block<'bType, 'b0, 'b1, 'b2, 'b3, 'b4, 'b5>(op0,op1,op2,op3) =
-    let b0 = new OperationBlock<'bType>(op0)
-    let b1 = new OperationBlock<'bType>(op1)
-    let b2 = new OperationBlock<'bType>(op2)
-    let b3 = new OperationBlock<'bType>(op3)
-    member this.GetVal(i, j) = 
-        match i with
-        | 0 -> b0.Get j
-        | 1 -> b1.Get j
-        | 2 -> b2.Get j
-        | 3 -> b3.Get j
-        | _ -> failwithf "Unexpected operation block number: %A"  i
+type Block<'bType>(ops:array<_>) =
+    let blocks = Array.init ops.Length (fun i -> new OperationBlock<'bType>(ops.[i]))
+    
+    member this.GetVal(i, j) = blocks.[i].Get j
     member val Instructions = new System.Collections.Generic.Queue<Instruction<'bType>>() with get,set
-    member val InConnectors = Unchecked.defaultof<ConnectorsBlock<'b0,'bType,'b1,'bType,'b2,'bType,'b3,'bType,'b4,'bType,'b5,'bType>>
-    member val OutConnectors = Unchecked.defaultof<ConnectorsBlock<'bType, 'b0,'bType,'b1,'bType,'b2,'bType,'b3,'bType,'b4,'bType,'b5>>
+    member val InConnectors:array<unit->'bType> = [||] with get,set
+    member val OutConnectors:array<'bType->unit> = [||] with get,set
     member this.Step() =
         let instr = this.Instructions.Dequeue()
         match instr with
         | Eps -> ()                        
         | Set (OpBlock (i,j), OpBlock (k,l)) ->
-            let v = 
-                match i with
-                | 0 -> b0.Get j
-                | 1 -> b1.Get j
-                | 2 -> b2.Get j
-                | 3 -> b3.Get j
-                | _ -> failwithf "Unexpected operation block number: %A" i
-                 
-            match k with
-            | 0 -> b0.Set l v
-            | 1 -> b1.Set l v
-            | 2 -> b2.Set l v
-            | 3 -> b3.Set l v
-            | _ -> failwithf "Unexpected operation block number: %A" k
+            let v = blocks.[i].Get j                 
+            blocks.[k].Set l v
 
         | Set (Connector i, OpBlock (j,k)) ->
-            let v = 
-                match i with
-                | 0 -> this.InConnectors.c0.OutBuf
-                | 1 -> this.InConnectors.c1.OutBuf
-                | 2 -> this.InConnectors.c2.OutBuf
-                | 3 -> this.InConnectors.c3.OutBuf
-                | 4 -> this.InConnectors.c4.OutBuf
-                | 5 -> this.InConnectors.c5.OutBuf
-                | _ -> failwithf "Unexpected in connector number: %A" i                 
-
-            match j with
-            | 0 -> b0.Set k v
-            | 1 -> b1.Set k v
-            | 2 -> b2.Set k v
-            | 3 -> b3.Set k v
-            | _ -> failwithf "Unexpected operation block number: %A" j
+            let v = this.InConnectors.[i]()
+            blocks.[j].Set k v
             
         | Set (Const v, OpBlock (j,k)) ->
-            match j with
-            | 0 -> b0.Set k v
-            | 1 -> b1.Set k v
-            | 2 -> b2.Set k v
-            | 3 -> b3.Set k v
-            | _ -> failwithf "Unexpected operation block number: %A" j
+            blocks.[j].Set k v
 
         | Move (Connector i, Connector j) -> 
-            let v = 
-                match i with
-                | 0 -> this.InConnectors.c0.OutBuf
-                | 1 -> this.InConnectors.c1.OutBuf
-                | 2 -> this.InConnectors.c2.OutBuf
-                | 3 -> this.InConnectors.c3.OutBuf
-                | 4 -> this.InConnectors.c4.OutBuf
-                | 5 -> this.InConnectors.c5.OutBuf
-                | _ -> failwithf "Unexpected in connector number: %A" i
-                 
-            match i with
-            | 0 -> this.OutConnectors.c0.Move v
-            | 1 -> this.OutConnectors.c1.Move v
-            | 2 -> this.OutConnectors.c2.Move v
-            | 3 -> this.OutConnectors.c3.Move v
-            | 4 -> this.OutConnectors.c4.Move v
-            | 5 -> this.OutConnectors.c5.Move v
-            | _ -> failwithf "Unexpected out connector number: %A" i
-            
+            let v =  this.InConnectors.[i]()
+            this.OutConnectors.[i] v
+
         | Move (OpBlock (i,j), Connector k) ->
-            let v = 
-                match i with
-                | 0 -> b0.Get j
-                | 1 -> b1.Get j
-                | 2 -> b2.Get j
-                | 3 -> b3.Get j
-                | _ -> failwithf "Unexpected operation block number: %A" i
-                 
-            match k with
-            | 0 -> this.OutConnectors.c0.Move v
-            | 1 -> this.OutConnectors.c1.Move v
-            | 2 -> this.OutConnectors.c2.Move v
-            | 3 -> this.OutConnectors.c3.Move v
-            | 4 -> this.OutConnectors.c4.Move v
-            | 5 -> this.OutConnectors.c5.Move v
-            | _ -> failwithf "Unexpected out connector number: %A" k
+            let v = blocks.[i].Get j
+            this.OutConnectors.[k] v
             
         | Move (OpBlock (i,j), OpBlock (k,l)) ->
-            let v = 
-                match i with
-                | 0 -> b0.Get j
-                | 1 -> b1.Get j
-                | 2 -> b2.Get j
-                | 3 -> b3.Get j
-                | _ -> failwithf "Unexpected operation block number: %A" i
-                 
-            match k with
-            | 0 -> b0.Move l v
-            | 1 -> b1.Move l v
-            | 2 -> b2.Move l v
-            | 3 -> b3.Move l v
-            | _ -> failwithf "Unexpected operation block number: %A" k
+            let v = blocks.[i].Get j
+            blocks.[k].Move l v
 
         | Move (Connector i, OpBlock (j,k)) ->
-            let v = 
-                match i with
-                | 0 -> this.InConnectors.c0.OutBuf
-                | 1 -> this.InConnectors.c1.OutBuf
-                | 2 -> this.InConnectors.c2.OutBuf
-                | 3 -> this.InConnectors.c3.OutBuf
-                | 4 -> this.InConnectors.c4.OutBuf
-                | 5 -> this.InConnectors.c5.OutBuf
-                | _ -> failwithf "Unexpected in connector number: %A" i                 
-
-            match j with
-            | 0 -> b0.Move k v
-            | 1 -> b1.Move k v
-            | 2 -> b2.Move k v
-            | 3 -> b3.Move k v
-            | _ -> failwithf "Unexpected operation block number: %A" j
+            let v = this.InConnectors.[i]()
+            blocks.[j].Move k v
             
         | Move (Const v, OpBlock (j,k)) ->
-            match j with
-            | 0 -> b0.Move k v
-            | 1 -> b1.Move k v
-            | 2 -> b2.Move k v
-            | 3 -> b3.Move k v
-            | _ -> failwithf "Unexpected operation block number: %A" j
+            blocks.[j].Move k v
 
         | Move (Const v, Connector i) ->
-            match i with
-            | 0 -> this.OutConnectors.c0.Move v
-            | 1 -> this.OutConnectors.c1.Move v
-            | 2 -> this.OutConnectors.c2.Move v
-            | 3 -> this.OutConnectors.c3.Move v
-            | 4 -> this.OutConnectors.c4.Move v
-            | 5 -> this.OutConnectors.c5.Move v
-            | _ -> failwithf "Unexpected out connector number: %A" i
+            this.OutConnectors.[i] v
 
         | _ -> failwithf "Unexpected command: %A" instr
     
