172ceb1cb992c448d6d3c0c72c2e77ffee02fdad
GovorukhaOksana <gos_1996@mail.ru>
2014-09-17 00:18:30 +0400

    example1+test.

---
+++ Source/Govorukha/example/example/Program.fs
@@ -0,0 +1,20 @@
+﻿module arr
+
+open TTA.ASM
+open main
+ 
+let func a b d e f g h =  
+  
+      let arr2: Program = [|[|Set((0<ln>,2<col>),b); Mvc((0<ln>,2<col>),d); Mov((0<ln>,0<col>),(0<ln>,2<col>)); 
+                                Set((1<ln>,2<col>),e); Mvc((1<ln>,2<col>),f); Mov((0<ln>,0<col>),(1<ln>,2<col>)); Set((0<ln>,3<col>),g); 
+                                 Mov((0<ln>,3<col>),(0<ln>,0<col>)); Set((0<ln>,1<col>),a); Mov((0<ln>,1<col>),(0<ln>,3<col>)); Mvc((0<ln>,1<col>),h)|]|]
+      
+      TTA.Translator.asmToIL arr2 |> main.run |> ignore
+      main.getVal 0 1 
+        
+      // a - g / (b * d + e * f) - h
+      // 10 - 20 / (2 * 3 + 4 * 1)  - 7
+    
+func 10 2 3 4 1 20 7  |> printfn "%A"
+
+//func 1 3 4 -3 1 45 10 |> printfn "%A"   
---
+++ Source/Govorukha/test/test/Program.fs
@@ -0,0 +1,16 @@
+﻿open arr
+open TTA.ASM
+open NUnit.Framework
+
+
+[<TestFixture>]
+type Test() = 
+     
+     [<Test>]
+     member this.``res``() =
+        Assert.AreEqual(1, func 10 2 3 4 1 20 7 ) 
+
+
+     //[<Test>]
+     //member this.``res2``() =
+      //  Assert.AreEqual(-14, func 1 3 4 -3 1 45 10 )
