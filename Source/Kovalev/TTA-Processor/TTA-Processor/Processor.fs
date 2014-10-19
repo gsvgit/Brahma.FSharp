028c051ed05e83d7db1198dae3a3f609c3cb83ea
lares77 <lares77@yandex.ru>
2014-10-19 19:19:28 +0400

    Processor based on dictionaries

--- Source/Kovalev/TTA-Processor/TTA-Processor/Processor.fs
+++ Source/Kovalev/TTA-Processor/TTA-Processor/Processor.fs
@@ -1,29 +1,31 @@
 ﻿module Processor
 
-open Cell
+open System.Collections.Generic
 
-type Processor<'a>(functions: array<'a -> 'a -> 'a>) =
+type Cell<'a> (operation: 'a -> 'a -> 'a) =
+    
+    let mutable value = Unchecked.defaultof<'a>
+    let op = operation
+
+    member this.Value
+        with get() = value
+        and set(arg) = value <- arg
+    member this.ExecuteOp operand =
+        value <- op value operand
+
+type Processor<'a> (functions: array<'a -> 'a -> 'a>) =
     do
         if functions.Length = 0
         then raise (System.ArgumentException("Empty grid"))
-            
-    let grid = Array.init functions.Length (fun i -> ResizeArray<int * Cell<'a>>())
+ 
+    let grid = Array.init functions.Length (fun i -> Dictionary<int, Cell<'a>>())
 
-    let addCellOnUserRequest key col =
-        let currentCol = grid.[col]
-        if currentCol.Count = 0
-        then currentCol.Add (key, Cell(functions.[col]))
-        else
-            let index = currentCol.FindIndex (fun i -> key < fst i)
-            if index = -1 
-            then currentCol.Add (key, Cell(functions.[col]))
-            else currentCol.Insert (index - 1, (key, Cell(functions.[col])))
+    let addCellOnUserRequest key col = grid.[col].Add (key, Cell(functions.[col]))
 
     member this.ValueInCell row col =
-        let index = grid.[col].FindIndex (fun i -> row = fst i)                
-        if index <> -1
-        then (snd grid.[col].[index]).Value 
-        else 
+        let currentCol = grid.[col]
+        if currentCol.ContainsKey row
+        then currentCol.[row].Value
+        else
             addCellOnUserRequest row col
-            let indexOfNew = grid.[col].FindIndex (fun i -> row = fst i) 
-            (snd grid.[col].[indexOfNew]).Value
+            currentCol.[row].Value
--- Source/Kovalev/TTA-Processor/Test/Tests.fs
+++ Source/Kovalev/TTA-Processor/Test/Tests.fs
@@ -4,24 +4,24 @@ open Processor
 open NUnit.Framework
 
 [<TestFixture>]
-type ProcessorTests() =    
+type ProcessorTests() =
 
     [<Test>]
     member this.``BuildingForInt``() =
-        let processor = new Processor<int>([|(fun x y -> x); (fun x y -> y)|])
-        Assert.AreEqual(0, processor.ValueInCell 1024 1)
+        let processor = new Processor<_>([|(fun x y -> x); (fun x y -> y)|])
+        Assert.AreEqual (0, processor.ValueInCell 1024 1)
 
     [<Test>]
     member this.``BuildingForBool``() =
         let processor = new Processor<bool>([|(fun x y -> x); (fun x y -> y)|])
-        Assert.AreEqual(false, processor.ValueInCell 2048 0)
+        Assert.AreEqual (false, processor.ValueInCell 2048 0)
 
     [<Test>]
     member this.``BuildingForByte``() =
         let processor = new Processor<byte>([|(fun x y -> x); (fun x y -> y)|])
-        Assert.AreEqual(0, processor.ValueInCell 4096 1)
+        Assert.AreEqual (0uy, processor.ValueInCell 4096 1)
 
     [<Test>]
     member this.``BuildingForStringArray``() =
         let processor = new Processor<array<string>>([|(fun x y -> x); (fun x y -> y)|])
-        Assert.AreEqual(Unchecked.defaultof<array<string>>, processor.ValueInCell 0 0)
+        Assert.AreEqual (Unchecked.defaultof<array<string>>, processor.ValueInCell 0 0)
