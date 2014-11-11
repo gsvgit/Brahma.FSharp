cfb0bf1b3a8635312333a22ba7dd6274b2a9f8b9
AndreyBulgakov <pivolit@gmail.com>
2014-11-12 01:18:33 +0300

    Processor Added

---
+++ Source/Bulgakov/MyProcessor/MyProcessor/Processor.fs
@@ -0,0 +1,93 @@
+﻿module Processor
+
+open System.Collections.Concurrent
+open System.Collections.Generic
+open TTA.ASM
+open Cell
+
+exception ParallelException of int*int
+type Processor<'a> (functions: array<'a -> 'a -> 'a>) =
+    do
+        if functions.Length = 0
+        then raise(System.ArgumentNullException())
+                 
+    let grid = Array.init functions.Length (fun i -> ConcurrentDictionary<int, Cell<'a>>())
+    let addCell row column = grid.[column].TryAdd(row, Cell(functions.[column]))
+    let usingCells = new HashSet<(int*int)>()
+    let WaitingList = new List<Asm<'a>>()
+
+    let execute command = 
+        match command with
+        |Set ((row1, col1), arg) ->
+            let row, col = int row1, int col1
+            
+            if not(grid.[col].ContainsKey row)
+            then
+                addCell row col
+                grid.[col].[row].Value <- arg
+            else
+                grid.[col].[row].Value <- arg
+        |Mov((row2, col2), (row1, col1)) ->
+            let toRow, toCol, fromRow, fromCol = int row2, int col2, int row1, int col1
+            
+            if not(grid.[toCol].ContainsKey toRow)
+            then
+                addCell toRow toCol
+                ()
+
+            if not(grid.[fromCol].ContainsKey fromRow)
+            then
+                addCell fromRow fromCol
+                ()
+
+            grid.[toCol].[toRow].Execute grid.[fromCol].[fromRow].Value
+
+        |Mvc((row1, col1), arg) ->
+            let row, col = int row1, int col1
+            if not(grid.[col].ContainsKey row)
+            then
+                addCell row col
+                grid.[col].[row].Execute arg
+            else
+                grid.[col].[row].Execute arg
+        |Eps -> ()
+
+    member this.ValueAt row col =
+        if not(grid.[col].ContainsKey row)
+        then
+            addCell row col
+            grid.[col].[row].Value
+        else grid.[col].[row].Value
+    
+    member this.executeLine(line : array<Asm<'a>>) =
+        for operation in line do
+            match operation with
+            |Set((row, col), arg)
+            |Mvc((row, col), arg) ->
+                if usingCells.Contains (int row, int col)
+                then raise(ParallelException (int row, int col))
+                else
+                    usingCells.Add (int row, int col)
+                    execute operation
+                    usingCells.Remove(int row, int col)
+            |Mov((row2, col2), (row1, col1)) ->
+                if usingCells.Contains (int row1, int col1)
+                then raise(ParallelException (int row1, int col1))
+                elif usingCells.Contains (int row2, int col2)
+                then raise(ParallelException (int row2, int col2))
+                else
+                    usingCells.Add (int row1, int col1)
+                    usingCells.Add (int row2, int col2)
+                    execute operation
+                    usingCells.Remove(int row1, int col1)
+                    usingCells.Remove(int row2, int col2)
+
+    member this.countCells =
+        let mutable count = 0
+        for i in grid do
+            for j in i do
+            count <- count + 1
+        count
+
+    member this.executeArray(arr : array<array<Asm<'a>>>) =
+        Array.Parallel.iter this.executeLine arr
---
+++ Source/Bulgakov/MyProcessor/ProcessorTests/ProcessorTests.fs
@@ -0,0 +1,70 @@
+﻿module NUnitTests
+
+open TTA.ASM
+open NUnit.Framework
+open Processor
+  
+[<TestFixture>]
+type CreateProcessor() =
+            
+    
+    [<Test>]
+    member this.``Create abstract processor``() =        
+        let processor = new Processor<_>([|(fun x y -> x); (fun x y-> y);|])
+        Assert.AreEqual(0, processor.ValueAt 186 0)
+    [<Test>]
+    member this.``Create integer processor``() =        
+        let processor = new Processor<int>([|(fun x y -> x+1); (fun x y-> x*x);|])
+        Assert.AreEqual(0, processor.ValueAt 186 0)
+    [<Test>]
+    member this.``Create boolean processor``() =        
+        let processor = new Processor<_>([|(fun x y -> x||y); (fun x y-> x&&y);|])
+        Assert.AreEqual(false, processor.ValueAt 186 0)
+    [<Test>]
+    member this.``Create byte processor``() =        
+        let processor = new Processor<byte>([|(fun x y -> x+y); (fun x y-> x*y);|])
+        Assert.AreEqual(0uy, processor.ValueAt 186 0)
+    [<Test>]
+    member this.``Create array<string> processor``() =        
+        let processor = new Processor<array<string>>([|(fun x y -> x); (fun x y-> y);|])
+        Assert.AreEqual(null, processor.ValueAt 186 0)
+
+[<TestFixture>]
+type ExecuteLineTests() =
+    [<Test>]
+    member this.``LineInt``() =
+        let processor = new Processor<int>([|(fun x y -> x+y); (fun x y-> x*x);|])
+        processor.executeLine [|Set((0<ln>, 0<col>), 1); Mvc((0<ln>, 0<col>), 5)|]
+        Assert.AreEqual(6, processor.ValueAt 0 0)
+
+
+[<TestFixture>]
+type ProcessorRunTests() =
+    
+    [<Test>]
+    member this.``EmptyArray``() =
+        let processor = new Processor<_>([|(fun x y -> x); (fun x y -> y)|])        
+        processor.executeArray [| [||]; [||] |]
+        Assert.AreEqual (0, processor.ValueAt 1024 1)
+    [<Test>]
+    member this.``ParallelTest``() =
+        let processor = new Processor<int>([|(fun x y -> x + y); (fun x y -> x + y)|])
+        processor.executeArray [| [|Set((0<ln>, 0<col>), 1); Mvc((0<ln>, 0<col>), 5)|]; 
+                         [|Set((0<ln>, 1<col>), 2); Mvc((0<ln>, 1<col>), 3)|] |]
+        Assert.AreEqual (6, processor.ValueAt 0 0)
+        Assert.AreEqual (5, processor.ValueAt 0 1) 
+    [<Test>]
+    member this.``HardParallelTest``() =
+        let processor = new Processor<int>([|(fun x y -> x + y); (fun x y -> x + y)|])
+        processor.executeArray [| [|Set((0<ln>, 0<col>), 1); Mvc((0<ln>, 0<col>), 5)|]; 
+                         [|Set((0<ln>, 1<col>), 2); Mvc((0<ln>, 1<col>), 3)|];
+                         [|Mov((0<ln>, 0<col>), (0<ln>, 1<col>))|]  |]
+        Assert.AreEqual (11, processor.ValueAt 0 0)
+
+    [<Test>]
+    member this.``HardParallelTest2``() =
+        let processor = new Processor<int>([|(fun x y -> x + y); (fun x y -> x + y)|])
+        processor.executeArray [| [|Set((0<ln>, 0<col>), 1); Mvc((0<ln>, 0<col>), 5)|]; 
+                         [|Set((0<ln>, 1<col>), 2); Mvc((0<ln>, 1<col>), 3)|];
+                         [|Mov((0<ln>, 0<col>), (0<ln>, 1<col>))|]  |]
+        Assert.AreEqual (11, processor.ValueAt 0 0)
