528a50d686b7a10f2ceccb11b34b0d33031e8f21
GrigorevSergey <unstope@gmail.com>
2014-12-08 23:35:21 +0300

    Final version

--- Source/Grigorev/Processor/Tests/AsmCompilerTests.fs
+++ Source/Grigorev/Processor/Tests/AsmCompilerTests.fs
@@ -70,23 +70,22 @@ type public YaccTests () =
                 "mvc 8 6     56-7"
             |] |> String.concat "\r\n"
         let code = [| thr |]
-        let exRes =
-            [|[|
-                Some (Asm.Eps);
-                Some (Asm.Set ((5<ln>, 3<col>), 7));
-                Some (Asm.Mvc ((0<ln>, 8<col>), -34));
-                Some (Asm.Mov ((0<ln>, 9<col>), (7<ln>, 6<col>)));
-                None;
-                None;
-                Some (Asm.Set ((6<ln>, -7<col>), 3));
-                None;
-                None;
-                Some (Asm.Set ((0<ln>, 0<col>), 0));
-                None;
-                Some (Asm.Mov ((0<ln>, 8<col>), (66<ln>, 9<col>)));
-                None;
-                None;
-                None
-            |]|]
         let comp = new AsmYaccCompiler<int> ()
-        Assert.That (comp.Compile code, Is.EqualTo (exRes))
+        Assert.That ((fun () -> match comp.Compile code with | Error _ -> true | _ -> false), Is.EqualTo(true))
+
+    [<Test>]
+    member this.Test1 () =
+        let thr = 
+            [|
+                "ePs";
+                "Set 5 3 7 ";
+                "mvc 0 8    -34   ";
+                "\tmov 0 9 7 6   ";
+                "";
+                "set 6 -7 3";
+                "SET 0 0 0"
+                "mov  0 8 66 9"
+            |] |> String.concat "\r\n"
+        let code = [| thr |]
+        let comp = new AsmYaccCompiler<int> ()
+        Assert.That ((fun () -> match comp.Compile code with | Error _ -> true | _ -> false), Is.EqualTo(false))
