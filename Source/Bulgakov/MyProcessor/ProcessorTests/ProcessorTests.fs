4f18f66e85d737678a3eeb03e37af70f41b10bd7
AndreyBulgakov <pivolit@gmail.com>
2014-12-08 23:59:19 +0300

    sdas

--- Source/Bulgakov/MyProcessor/ProcessorTests/ProcessorTests.fs
+++ Source/Bulgakov/MyProcessor/ProcessorTests/ProcessorTests.fs
@@ -69,3 +69,20 @@ type ProcessorRunTests() =
         let processor = new Processor<int>([|(fun x y -> x + y); (fun x y -> x - y); (fun x y -> x * y); (fun x y -> x / y)|])
         processor.executeProgram [| [|Set((100<ln>, 2<col>), 1);|] |]
         Assert.AreEqual(1, processor.ValueAt 100 2)
+
+// 
+// [<Test>]
+// member this.``InVarTest``() =
+// let Processor = new Processor<MyType>([|(fun x y -> x); (fun x y -> y)|])
+// let Processor1 = new Processor<MyType1>([|(fun x y -> x); (fun x y -> y)|])
+// let func (m:Processor<MyType>) =
+// (m.ValueInCell 0 0).sout
+// Assert.AreEqual(Unchecked.defaultof<MyType>, func ((Processor<MyType>) Processor1))
+//
+// [<Test>]
+// member this.``ContrVarTest``() =
+// let Processor = new Processor<MyType>([|(fun x y -> x); (fun x y -> y)|])
+// let Processor1 = new Processor<MyType1>([|(fun x y -> x); (fun x y -> y)|])
+// let func (m:Processor<MyType1>) =
+// (m.ValueInCell 0 0).sout
+// Assert.AreEqual(Unchecked.defaultof<MyType1>, func (Processor))
