cce82ad92fb3e6eee75093ca9d28cdbce069ab96
VereshchaginaE <kryska18@mail.ru>
2014-12-14 22:35:48 +0300

    Parser

--- Source/Vereshchagina/TTA/Test/Program.fs
+++ Source/Vereshchagina/TTA/Test/Program.fs
@@ -1,5 +1,6 @@
 ﻿open TTA.Processor
 open TTA.ASM
+open TTA.Parser
 
 let processor = new Processor<_>([|(fun x y -> x + y); (fun x y -> x + y)|])
 for i in 0..100 do
@@ -7,4 +8,10 @@ for i in 0..100 do
                      [|Set((0<ln>, 1<col>), 3); Mvc((0<ln>, 1<col>), 4)|];
                      [|Eps|]|]
          
-    processor.ValueInCell 0 0 |> printfn "%A"       
+    processor.ValueInCell 0 0 |> printfn "%A"
+    
+let x  = parse "Set (0 0 3) Mov (0 0 1 1);
+                Set (0 0 4) Eps;
+                Eps         Eps           Eps        Mvc (0 0 3);"
+
+printfn "%A" x
