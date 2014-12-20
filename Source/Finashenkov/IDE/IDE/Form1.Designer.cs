ba01d04965cc0e5d053cae11b989b132b75f44b3
NikolayFin <finashenkov.brazas@yandex.ru>
2014-12-21 00:06:33 +0300

    All done

---
+++ Source/Finashenkov/IDE/IDE/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿using System.Windows.Forms;
+
+namespace IDE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.startButton = new System.Windows.Forms.Button();
+            this.debugButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.errorBox = new System.Windows.Forms.ListBox();
+            this.stepButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Location = new System.Drawing.Point(12, 30);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(465, 325);
+            this.textBox.TabIndex = 0;
+            this.textBox.Text = "";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView.Location = new System.Drawing.Point(481, 30);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersWidth = 64;
+            this.dataGridView.Size = new System.Drawing.Size(466, 325);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Addition";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Substraction";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Multiplication";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Divison";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.startButton.BackColor = System.Drawing.SystemColors.Control;
+            this.startButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.startButton.Location = new System.Drawing.Point(367, 1);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 3;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            // 
+            // debugButton
+            // 
+            this.debugButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.debugButton.BackColor = System.Drawing.SystemColors.Control;
+            this.debugButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.debugButton.Location = new System.Drawing.Point(448, 1);
+            this.debugButton.Name = "debugButton";
+            this.debugButton.Size = new System.Drawing.Size(75, 23);
+            this.debugButton.TabIndex = 3;
+            this.debugButton.Text = "Debug";
+            this.debugButton.UseVisualStyleBackColor = false;
+            // 
+            // stopButton
+            // 
+            this.stopButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.stopButton.BackColor = System.Drawing.SystemColors.Control;
+            this.stopButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.stopButton.Location = new System.Drawing.Point(622, 1);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Visible = false;
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(959, 24);
+            this.menuStrip.TabIndex = 4;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // errorBox
+            // 
+            this.errorBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.errorBox.FormattingEnabled = true;
+            this.errorBox.Location = new System.Drawing.Point(12, 371);
+            this.errorBox.Name = "errorBox";
+            this.errorBox.Size = new System.Drawing.Size(935, 95);
+            this.errorBox.TabIndex = 5;
+            // 
+            // stepButton
+            // 
+            this.stepButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.stepButton.BackColor = System.Drawing.SystemColors.Control;
+            this.stepButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.stepButton.Location = new System.Drawing.Point(541, 1);
+            this.stepButton.Name = "stepButton";
+            this.stepButton.Size = new System.Drawing.Size(75, 23);
+            this.stepButton.TabIndex = 3;
+            this.stepButton.Text = "Next step";
+            this.stepButton.UseVisualStyleBackColor = false;
+            this.stepButton.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 468);
+            this.Controls.Add(this.errorBox);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.stepButton);
+            this.Controls.Add(this.debugButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "IDE";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textBox;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button debugButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private ListBox errorBox;
+        private Button stepButton;
+    }
+}
+
---
+++ Source/Finashenkov/IDE/IDE/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace IDE
+{
+    public partial class Form1 : Form
+    {
+        Compiler.Compiler comp = new Compiler.Compiler();
+        int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            var open = Observable.FromEventPattern(h => openToolStripMenuItem.Click += h, h => openToolStripMenuItem.Click -= h);
+            open.ObserveOn(SynchronizationContext.Current).Subscribe(x => OpenFile(openToolStripMenuItem));
+            var save = Observable.FromEventPattern(h => saveToolStripMenuItem.Click += h, h => saveToolStripMenuItem.Click -= h);
+            save.ObserveOn(SynchronizationContext.Current).Subscribe(x => SaveFile(saveToolStripMenuItem));
+            var formClosing = Observable.FromEventPattern<FormClosingEventHandler, FormClosingEventArgs>(h => FormClosing += h, h => FormClosing -= h);
+            formClosing.Subscribe(x => Closing(x.EventArgs));
+            var start = Observable.FromEventPattern(h => startButton.Click += h, h => startButton.Click -= h);
+            start.ObserveOn(SynchronizationContext.Current).Subscribe(x => Start(startButton));
+            var debug = Observable.FromEventPattern(h => debugButton.Click += h, h => debugButton.Click -= h);
+            debug.ObserveOn(SynchronizationContext.Current).Subscribe(x => Debug(debugButton));
+            var step = Observable.FromEventPattern(h => stepButton.Click += h, h => stepButton.Click -= h);
+            step.ObserveOn(SynchronizationContext.Current).Subscribe(x => NextStep(stepButton));
+            var stop = Observable.FromEventPattern(h => stopButton.Click += h, h => stopButton.Click -= h);
+            stop.ObserveOn(SynchronizationContext.Current).Subscribe(x => Stop(stopButton));
+            var num = Observable.FromEventPattern<DataGridViewRowsAddedEventHandler, DataGridViewRowsAddedEventArgs>(h => dataGridView.RowsAdded += h, h => dataGridView.RowsAdded -= h);
+            num.ObserveOn(SynchronizationContext.Current).Subscribe(x => NumerateDataGrid(x.EventArgs));
+        }
+        private void Open()
+        {
+            string filePath = "";
+            OpenFileDialog Fd = new OpenFileDialog();
+            Fd.Filter = "txt files (*.txt)|*.txt";
+            if (Fd.ShowDialog() == DialogResult.OK)
+            {
+                filePath = Fd.FileName;
+                string str = System.IO.File.ReadAllText(@filePath);
+                textBox.Text = str;
+            }
+        }
+        private bool SaveFile(object sender)
+        {
+            string filePath = "";
+            string str = textBox.Text;
+
+            SaveFileDialog Sd = new SaveFileDialog();
+            Sd.Filter = "txt files (*.txt)|*.txt";
+            if (Sd.ShowDialog() == DialogResult.OK)
+            {
+                filePath = Sd.FileName;
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(filePath);
+                sw.Write(textBox.Text);
+                sw.Close();
+                return true;
+            }
+            return false;
+        }
+        
+        private bool SaveOnClose()
+        {
+            var dr = MessageBox.Show("Do you want save program before exit?", "Alert", MessageBoxButtons.YesNoCancel);
+            if (dr == DialogResult.Yes)
+            {
+                SaveFile(saveToolStripMenuItem);
+                return false;
+            }
+            if (dr == DialogResult.No)
+            {
+                return false;
+            }
+            return true;
+        }
+        private void Closing(FormClosingEventArgs e)
+        {
+            e.Cancel = SaveOnClose(); ;
+        }
+        private void CreateDataGrid(Compiler.Compiler compiler, DataGridView dataGridView1)
+        {
+            dataGridView1.RowCount = compiler.CountRows();
+            for (int i = 0; i < compiler.CountCols(); i++)
+            {
+                Dictionary<int, string> cells = compiler.GetStringGrid(i);
+                foreach (KeyValuePair<int, string> kvp in cells)
+                {
+                    dataGridView1[i, kvp.Key].Value = kvp.Value;
+                }
+            }
+        }
+        private void NumerateDataGrid(DataGridViewRowsAddedEventArgs e)
+        {
+            for (int i = 0; i < dataGridView.Rows.Count; i++)
+            {
+                dataGridView.Rows[i].HeaderCell.Value = String.Format("{0}", dataGridView.Rows[i].Index);
+            }
+            dataGridView.AutoResizeRowHeadersWidth(0, DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+        }
+        public List<string> createErrorBox()
+        {
+            HashSet<Tuple<String, int, int>> errs = comp.GetErrorsList();
+            List<string> _items = new List<string>();
+            foreach (Tuple<String, int, int> i in errs)
+            {
+                _items.Add(i.Item1 + " in code line " + (i.Item2 + 1) + " operation " + (i.Item3 + 1));
+            }
+            dataGridView.Rows.Clear();
+            this.errorBox.DataSource = _items;
+            return _items;
+        }
+        private void Start(object sender)
+        {
+            try
+            {
+                errorBox.DataSource = new List<string>();
+                dataGridView.Rows.Clear();
+                comp.Stop();
+                comp.Compile(textBox.Text);
+                comp.Run();
+                this.CreateDataGrid(comp, dataGridView);
+            }
+            catch (Compiler.CompileException e)
+            {
+                List<String> list = new List<String>();
+                list.Add(e.Message);
+                errorBox.DataSource = list;
+                comp.Stop();
+            }
+            catch (Compiler.RunTimeException e)
+            {
+                List<String> list = new List<String>();
+                list.Add(e.Message);
+                errorBox.DataSource = list;
+                comp.Stop();
+            }
+            catch (Exception e)
+            {
+                createErrorBox();
+            }
+        }
+        private void Debug(object sender)
+        {
+            dataGridView.Rows.Clear();
+            comp.Stop();
+            try
+            {
+                comp.Compile(textBox.Text);
+                comp.Debug();
+                startButton.Visible = false;
+                debugButton.Visible = false;
+                stepButton.Visible = true;
+                stopButton.Visible = true;
+                textBox.Enabled = false;
+                errorBox.DataSource = new List<string>();
+            }
+            catch (Compiler.CompileException e)
+            {
+                List<String> list = new List<String>();
+                list.Add(e.Message);
+                errorBox.DataSource = list;
+            }
+            catch (Exception e)
+            {
+                createErrorBox();
+            }
+        }
+        private void NextStep(object sender)
+        {
+            if (count < textBox.Lines.Length)
+            {
+                Highlight();
+                textBox.Show();
+                comp.Step(count);
+                count++;
+                this.CreateDataGrid(comp, dataGridView);
+            }
+            else
+            {
+                Stop(sender);
+            }
+        }
+        private void Stop(object sender)
+        {
+            textBox.SelectAll();
+            textBox.SelectionBackColor = System.Drawing.Color.White;
+            textBox.SelectionColor = Color.Black;
+            startButton.Visible = true;
+            debugButton.Visible = true;
+            stepButton.Visible = false;
+            stopButton.Visible = false;
+            textBox.Enabled = true;
+            comp.Stop();
+            dataGridView.Rows.Clear();
+            count = 0;
+        }       
+        private void Highlight()
+        {
+            //Clear previous
+            textBox.Select(0, textBox.GetFirstCharIndexFromLine(count));
+            textBox.SelectionColor = System.Drawing.Color.Black;
+            textBox.SelectionBackColor = System.Drawing.Color.WhiteSmoke;
+
+            //Char position
+            int firstCharPosition = textBox.GetFirstCharIndexFromLine(count);
+            int ln = textBox.Lines[count].Length;
+
+            //Select
+            textBox.Select(firstCharPosition, ln);
+            textBox.Select();
+
+            //Select Color
+            textBox.SelectionColor = System.Drawing.Color.White;
+            textBox.SelectionBackColor = System.Drawing.Color.Blue;
+
+        }
+        private void OpenFile(object sender)
+        {
+            try
+            {
+                var dr = MessageBox.Show("Do you want save program before open?", "Alert", MessageBoxButtons.YesNoCancel);
+                if (dr == DialogResult.Yes)
+                {
+                    if (SaveFile(saveToolStripMenuItem))
+                    {
+                        Open();
+                    }
+                }
+                if (dr == DialogResult.No)
+                {
+                    Open();
+                }
+            }
+            catch (Exception) { };
+        }      
+    }
+}
+
