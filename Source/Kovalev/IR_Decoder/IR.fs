a91144d2c16cf441df82bb15fe9e56a15adf9c19
lares77 <lares77@yandex.ru>
2015-04-26 02:42:31 +0300

    Blocks with Zip. Examples as tests.

--- Source/Kovalev/IR_Decoder/IR.fs
+++ Source/Kovalev/IR_Decoder/IR.fs
@@ -11,12 +11,21 @@ type Types =
     | Int of int
     | Bool of bool
 
-(*type Block<'I, 'O> (input: IObservable<'I>, op: 'I -> 'O) =    
-    member val Output = Observable.map op input with get*)
+type Block<'I, 'O> (input: IObservable<'I>, op: 'I -> 'O) =    
+    member val Output = Observable.map op input with get
+
+type Block<'I1, 'I2, 'O> (input1: IObservable<'I1>, input2: IObservable<'I2>, op: 'I1 * 'I2 -> 'O) =
+    member val Output = Observable.zip input1 input2 |> Observable.map op with get
+
+type Block<'I1, 'I2, 'I3, 'O> (input1: IObservable<'I1>, input2: IObservable<'I2>, input3: IObservable<'I3>, 
+                               op: 'I1 * 'I2 * 'I3 -> 'O) =
+    member val Output = Observable.zip3 input1 input2 input3 |> Observable.map op with get
+
+
 
 type Block(inputs: list<IObservable<Types>>, op: Types -> Types -> Types) =    
     member val Output = Observable.zipSeqMap (fun list -> op list.[1] list.[0]) inputs
-    //member val Output = Observable.zip input1 input2 |> Observable.map op
+ 
          
 
 (*type VSFGraph<'T>() =
--- Source/Kovalev/Tests/Program.fs
+++ Source/Kovalev/Tests/Program.fs
@@ -7,15 +7,40 @@ open Builders
 open NUnit.Framework
 open IR
 
-(*let x = new Block<int, int> (Observable.Return 42, fun x -> x + 1)
 
-let x2 = new Block<int, bool> (x.Output, fun x -> if x % 2 <> 0 then true else false)*)
+[<TestFixture>]
+type TestBlocks () =
+    [<Test>]
+    member this.``simpleIncrement`` () =
+        let value = Observable.Return 1
+        
+        let inc = new Block<int, int> (value, fun x -> x + 1)        
 
-(*let y = x2.Output
-y.Subscribe(fun x -> x.ToString() |> printfn "%A") |> ignore*)
+        let actual = ref 0
+        inc.Output.Subscribe (fun x -> actual := x) |> ignore
 
-let x3 = new Block ([Observable.Return (Types.Int 42); Observable.Return (Types.Int 42)], fun x y -> match x, y with 
-                                                                                                     Types.Int a, Types.Int b -> Types.Int (a + b)
-                                                                                                    | _ -> failwith "hello" )
+        Assert.AreEqual (2, !actual)
+    
+    [<Test>]
+    member this.``workWithPredicate`` () =
+        let first = Observable.Return 1
+        let second = Observable.Return 2
 
-x3.Output.Subscribe(fun x -> match x with Types.Int a -> a.ToString() |> printfn "%A" | _ -> failwith "") |> ignore
+        let predicate = new Block<int, bool> (second, fun x -> if x % 2 = 0 then true else false)
+        let usefulFunc = new Block<int, int, bool, int> (first, second, predicate.Output, fun (x, y, b) -> if b then x + y else x * y)
+
+        let actual = ref 0
+        usefulFunc.Output.Subscribe (fun x -> actual := x) |> ignore
+
+        Assert.AreEqual (3, !actual)
+
+
+    [<Test>]
+    member this.``PlsNoOhMyEyesICan'tSee`` () =
+        let add = new Block ([Observable.Return (Types.Int 42); Observable.Return (Types.Int 42)], fun x y -> match x, y with 
+                                                                                                        Types.Int a, Types.Int b -> Types.Int (a + b)
+                                                                                                        | _ -> failwith "hello" )        
+        let actual = ref 0
+        add.Output.Subscribe(fun x -> match x with Types.Int a -> actual := a | _ -> failwith "") |> ignore
+        
+        Assert.AreEqual (84, !actual)        
