e8380cf96d822bb3b3ab117876831696eda9651b
gsv <rsdpisuy@gmail.com>
2015-06-16 15:06:57 +0300

    First version of vsfg.

---
+++ Source/ASM/VSFG.fs
@@ -0,0 +1,62 @@
+﻿module TTA.VSFG
+
+open QuickGraph
+
+type IVSFGNode = interface end
+
+type Port () = 
+    interface IVSFGNode
+
+type InPort (inputs:ResizeArray<OutPort>) =
+    inherit Port ()
+    member this.Inputs = inputs
+    member this.ConnectWith p = this.Inputs.Add p
+    new () = InPort (new ResizeArray<_>())
+    new (x:OutPort) = InPort (new ResizeArray<_>([|x|]))
+
+and OutPort (targets:ResizeArray<InPort>) =
+    inherit Port ()
+    member this.Targets = targets
+    member this.ConnectWith p = this.Targets.Add p
+    new () = OutPort (new ResizeArray<_>())
+    new (x:InPort) = OutPort (new ResizeArray<_>([|x|]))
+
+//type VarPort = 
+//    inherit Port
+//
+//type StatePort = 
+//    inherit Port
+//
+//type PredPort = 
+//    inherit Port
+
+type VSFGBlock (inPorts:ResizeArray<InPort>, outPort:OutPort) = 
+    interface IVSFGNode    
+    member this.OutPort = outPort 
+    member this.InPorts = inPorts
+
+type Multiplexor (predicate:InPort, _true:InPort, _false:InPort, out) = 
+    inherit VSFGBlock(new ResizeArray<_>([|predicate; _true; _false|]) , out)
+    member this.Predicate = predicate
+    member this.True = _true
+    member this.False = _false
+
+type UnOp = 
+    | Minus
+    | AddC of int
+
+type BinOp =
+    | Sum
+    | Div
+    | Mult
+    | Eq
+    | Less
+    | Gr
+    | LEq
+    | GEq
+
+type BinOpBlock(l:InPort, r:InPort, op:BinOp, out) =
+    inherit VSFGBlock(new ResizeArray<_>([|l;r|]), out)
+
+type UnOpBlock(input:InPort, op:UnOp, out) =
+    inherit VSFGBlock(new ResizeArray<_>([|input|]), out)
--- Source/Example/Program.fs
+++ Source/Example/Program.fs
@@ -1,20 +1,57 @@
-﻿open TTA.ASM
+﻿open TTA.VSFG
 
 (*
-board:
-  | 0 | 1 | 2 | 3
------------------
-0 | + | - | * | /
------------------
-1 | + | - | * | /
------------------
-2 | + | - | * | /
------------------
-3 | + | - | * | /
------------------
+let rec f x y =
+    if x < y 
+    then f x + 1 y
+    else x + y
 *)
-do
-    //asm for (1 + 2) * 3
-    let asm = [|[|Set((0<ln>, 0<col>), 1); Mvc((0<ln>, 0<col>), 2); Set((0<ln>, 2<col>),3); Mov((0<ln>, 2<col>),(0<ln>, 0<col>));|]|]
-    main.run (TTA.Translator.asmToIL asm) |> ignore
-    main.getVal 0 2 |> (printfn "result: %A")
+
+// x < y
+
+let _x1 = new InPort()
+let _y1 = new InPort()
+let _x_y1 = new OutPort()
+let x_l_y = new BinOpBlock(_x1,_y1,BinOp.Less,_x_y1)
+
+//x+y
+
+let _x2 = new InPort()
+let _y2 = new InPort()
+let _x_y2 = new OutPort()
+let x_a_y = new BinOpBlock(_x2,_y2,BinOp.Sum,_x_y2)
+
+// x + 1
+
+let _x3 = new InPort()
+let _x_1 = new OutPort()
+let x_incr = new UnOpBlock(_x3,UnOp.AddC 1,_x_1)
+
+// if
+
+let pred = new InPort(_x_y1)
+let _true = new InPort()
+let _false = new InPort(_x_y2)
+let if_out = new OutPort()
+let _if = new Multiplexor(pred,_true,_false,if_out)
+
+//f
+
+let dummy_x_out = new OutPort()
+let dummy_y_out = new OutPort()
+
+let x_f = new InPort(dummy_x_out)
+let y_f = new InPort(dummy_y_out)
+let f_out = if_out
+
+dummy_x_out.ConnectWith _x3
+dummy_x_out.ConnectWith _x2
+dummy_x_out.ConnectWith _x1
+
+dummy_y_out.ConnectWith _y2
+dummy_y_out.ConnectWith _y1
+
+
+
+
+
